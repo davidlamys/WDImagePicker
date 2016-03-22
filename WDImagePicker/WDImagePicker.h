b22bac851adcc7a46fb1d310044574a85973f2f4
Russell Ong <mrrussellong@yahoo.com>
2016-03-22 10:22:53 +0800

    carthage support

---
+++ WDImagePicker/WDImagePicker.h
@@ -0,0 +1,19 @@
+//
+//  WDImagePicker.h
+//  WDImagePicker
+//
+//  Created by Russell Ong on 3/22/16.
+//  Copyright © 2016 Wu Di. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WDImagePicker.
+FOUNDATION_EXPORT double WDImagePickerVersionNumber;
+
+//! Project version string for WDImagePicker.
+FOUNDATION_EXPORT const unsigned char WDImagePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WDImagePicker/PublicHeader.h>
+
+
